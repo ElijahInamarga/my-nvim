06f759c34263c812adaff34f4f54462ca5d2ddda
ElijahInamarga <elijahcek@gmail.com>
2026-07-13 12:31:57 -0700

    reformated imports/requires

--- lua/pack.lua
+++ lua/pack.lua
@@ -87,6 +87,3 @@ MiniDiff.setup({
 
 vim.keymap.set("n", "<leader>gg", "<cmd>tabnew | Git | only<cr>", { desc = "Fugitive Full Page New Tab" })
 vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiffsplit<CR>", { desc = "Git diff split", })
-
-require("treesitter")
-require("lsp")
